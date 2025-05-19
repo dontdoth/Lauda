1914e1619ac8a1ad1e541409f68a774dde22fdf6
ησᴛ ᴄσᴅєʀ <shamansh3137@gmail.com>
2025-05-20 00:28:34 +0530

    Update play.py

--- PURVIMUSIC/utils/inline/play.py
+++ PURVIMUSIC/utils/inline/play.py
@@ -60,18 +60,18 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
-        #[
-           # InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            #InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            #InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            #InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            #InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        #],
-         [
-             InlineKeyboardButton(text="σᴡηєʀ", url="https://t.me/PURVI_SUPPORT"),
-             InlineKeyboardButton(text="๏ ᴧᴅᴅ ϻє ๏", url=f"https://t.me/{app.username}?startgroup=true"),
-             InlineKeyboardButton(text="ѕᴜᴘᴘσʀᴛ", url="https://t.me/+gMy8Cp190ediNzZl")
-         ],
+        [
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        # [
+           #  InlineKeyboardButton(text="σᴡηєʀ", url="https://t.me/PURVI_SUPPORT"),
+            # InlineKeyboardButton(text="๏ ᴧᴅᴅ ϻє ๏", url=f"https://t.me/{app.username}?startgroup=true"),
+           #  InlineKeyboardButton(text="ѕᴜᴘᴘσʀᴛ", url="https://t.me/+gMy8Cp190ediNzZl")
+         #],
         [
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ]
@@ -81,18 +81,18 @@ def stream_markup_timer(_, chat_id, played, dur):
 
 def stream_markup(_, chat_id):
     buttons = [
-        #[
-            #InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            #InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            #InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-           # InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-           # InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-         #],
         [
-             InlineKeyboardButton(text="σᴡηєʀ", url="https://t.me/PURVI_SUPPORT"),
-             InlineKeyboardButton(text="๏ ᴧᴅᴅ ϻє ๏", url=f"https://t.me/{app.username}?startgroup=true"),
-             InlineKeyboardButton(text="ѕᴜᴘᴘσʀᴛ", url="https://t.me/+gMy8Cp190ediNzZl")
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
          ],
+        #[
+            # InlineKeyboardButton(text="σᴡηєʀ", url="https://t.me/PURVI_SUPPORT"),
+           #  InlineKeyboardButton(text="๏ ᴧᴅᴅ ϻє ๏", url=f"https://t.me/{app.username}?startgroup=true"),
+            # InlineKeyboardButton(text="ѕᴜᴘᴘσʀᴛ", url="https://t.me/+gMy8Cp190ediNzZl")
+        # ],
         [
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ]
