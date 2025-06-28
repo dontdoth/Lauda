351c2abee74060b2134440ef1fa4755bb23a2249
ησᴛ ᴄσᴅєʀ <shamansh3137@gmail.com>
2025-06-28 14:55:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Im-Notcoder/Lauda",
+    "https://github.com/dontdoth/Lauda",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
